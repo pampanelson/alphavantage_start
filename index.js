9dbaa198bc072c6e24c575e48df96d5772f29c32
pampa nelson <pampa.nelson@gmail.com>
2018-10-29 14:15:51 +0800

    got full data

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ console.log("hello");
 var AlphaVantageAPI = require("alpha-vantage-cli").AlphaVantageAPI;
 
 var myApiKey = "RI830Q7FPOXVW3EW";
-var alphaVantageAPI = new AlphaVantageAPI(myApiKey, "compact", true);
+var alphaVantageAPI = new AlphaVantageAPI(myApiKey, "full", true);
 
-alphaVantageAPI.getIntradayData("MSFT", "15min")
+alphaVantageAPI.getIntradayData("600055.SS", "1min")
 	.then(intradayData => {
 		console.log("Intraday data:");
 		console.log(intradayData);
